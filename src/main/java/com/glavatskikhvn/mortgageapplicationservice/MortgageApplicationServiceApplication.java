54d33d618de333ceb7c27e1169edb5ecd803dc0b
Valerii Glavatskih <glavatskihvalerii@yandex.ru>
2021-10-31 20:58:49 +0400

    Fix packages

---
+++ src/main/java/com/glavatskikhvn/mortgageapplicationservice/MortgageApplicationServiceApplication.java
@@ -0,0 +1,23 @@
+package com.glavatskikhvn.mortgageapplicationservice;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.glavatskikhvn.mortgageapplicationservice.customer.CustomerRepository;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Mortgage Application",
+				description = "API ипотечных заявок",
+				version = "1.0"
+		)
+)
+@SpringBootApplication
+	public class MortgageApplicationServiceApplication extends SpringBootServletInitializer {
+	private CustomerRepository customerRepository;
+	public static void main(String[] args) {
+		SpringApplication.run(MortgageApplicationServiceApplication.class, args);
+	}
+}
---
+++ src/main/java/com/glavatskikhvn/mortgageapplicationservice/customer/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.glavatskikhvn.mortgageapplicationservice.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface CustomerRepository
+        extends JpaRepository<Customer, String> {
+    Customer findByFirstnameAndLastNameAndPatronymicAndPassportNumber(String firstname, String lastName,
+                                                                      String patronymic, String passportNumber);
+}
