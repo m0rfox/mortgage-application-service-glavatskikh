0528284d07988c2eb68157e0bad0327a3deb4b7e
Valerii Glavatskih <glavatskihvalerii@yandex.ru>
2021-10-31 20:58:22 +0400

    Fix packages

--- Mortgage-Application-Service/src/main/java/com/glavatskikhvn/mortgageapplicationservice/customer/Sex.java
+++
@@ -1,9 +0,0 @@
-package com.glavatskikhvn.mortgageapplicationservice.customer;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@Schema(enumAsRef = true)
-public enum Sex {
-    MALE,
-    FEMALE
-}
